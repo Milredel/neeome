bba92ff6fb3bdcc2a1f6ab5ec3fe6032993d3fde
morgan.fromentin <morgan.fromentin@ezbeez.eu>
2017-11-30 15:53:20 +0100

    starting things

---
+++ config.js
@@ -0,0 +1,19 @@
+//-------------------------------------------
+// Config initialization
+//-------------------------------------------
+let config = {};
+config.neeo = {};
+
+//-------------------------------------------
+// NEEO Settings
+//-------------------------------------------
+
+//neeo brain ip
+config.neeo.brain.ip = '192.168.0.41';
+
+//neeo api version
+config.neeo.api.version = 'v1';
+
+//-------------------------------------------
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-var http = require('http');
+const express = require('express');
+const fetch = require('node-fetch');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
-});
-server.listen(8080);
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', safeHandler(defaultHandler));
+app.get('/recipes', safeHandler(recipesHandler));
+
+app.listen(8080);
+
+function safeHandler(handler) {
+    return function(req, res) {
+        handler(req, res).catch(error => res.status(500).send(error.message));
+    };
+}
+
+async function defaultHandler(req, res) {
+    res.send('Welcome on this little server created to control NEEO recipes from Google Home via IFTTT');
+}
+
+async function recipesHandler(req, res) {
+    var getAllRecipesUrl = "http://"+config.neeo.brain.ip+":3000/"+config.neeo.api.version+"/api/Recipes";
+    try {
+        var response = await fetch(getAllRecipesUrl);
+        var recipes = await response.json();
+        res.render('recipes', { title: 'Existing NEEO Recipes', recipes: recipes })
+    } catch (e) {
+        req.send('Error from the recipesHandler (' + e +').');
+    }
+}
