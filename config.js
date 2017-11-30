764b2a980a289d4a2167c5dfd825cf637328613b
morgan.fromentin <morgan.fromentin@ezbeez.eu>
2017-11-30 18:33:09 +0100

    almost there

--- config.js
+++ config.js
@@ -3,16 +3,27 @@
 //-------------------------------------------
 let config = {};
 config.neeo = {};
+config.home = {};
+
+//-------------------------------------------
+// NEEO Settings
+//-------------------------------------------
+
+//your public ip or DNS
+config.home.public_dns = 'mypublicdns';
+
+//a private token to "authenticate" from IFTT
+config.home.private_token = 'myprivatetoken';
 
 //-------------------------------------------
 // NEEO Settings
 //-------------------------------------------
 
 //neeo brain ip
-config.neeo.brain.ip = '192.168.0.41';
+config.neeo.brain_ip = '192.168.0.41';
 
 //neeo api version
-config.neeo.api.version = 'v1';
+config.neeo.api_version = 'v1';
 
 //-------------------------------------------
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const config = require('./config.sample');
 
 const app = express();
 
@@ -7,12 +8,17 @@ app.set('view engine', 'pug');
 
 app.get('/', safeHandler(defaultHandler));
 app.get('/recipes', safeHandler(recipesHandler));
+app.get('/neeo', safeHandler(executeHandler));
 
 app.listen(8080);
 
 function safeHandler(handler) {
     return function(req, res) {
-        handler(req, res).catch(error => res.status(500).send(error.message));
+        if (req.query.token == undefined || req.query.token != config.home.private_token) {
+            res.status(403).send("unauthorized request");
+        } else {
+            handler(req, res).catch(error => res.status(500).send(error.message));
+        }
     };
 }
 
@@ -21,12 +27,28 @@ async function defaultHandler(req, res) {
 }
 
 async function recipesHandler(req, res) {
-    var getAllRecipesUrl = "http://"+config.neeo.brain.ip+":3000/"+config.neeo.api.version+"/api/Recipes";
+    var getAllRecipesUrl = "http://"+config.neeo.brain_ip+":3000/"+config.neeo.api_version+"/api/Recipes";
     try {
         var response = await fetch(getAllRecipesUrl);
         var recipes = await response.json();
+        recipes.forEach(function(recipe, index) {
+            recipes[index].url.distantSetPowerOn = formatDistantUrl(recipe.url.setPowerOn);
+            recipes[index].url.distantSetPowerOff = formatDistantUrl(recipe.url.setPowerOff);
+        });
         res.render('recipes', { title: 'Existing NEEO Recipes', recipes: recipes })
     } catch (e) {
         req.send('Error from the recipesHandler (' + e +').');
     }
+}
+
+function formatDistantUrl(url) {
+    var pattern = /[0-9]{8,}/;
+    var regex = new RegExp(pattern, "g");
+    var matches = url.match(regex);
+    var myToken = config.home.private_token;
+    return config.home.public_dns+"/neeo?token="+myToken+"&room="+matches[0]+"&recipe="+matches[1];
+}
+
+async function executeHandler(req, res) {
+    res.send('OK, I received something from IFTTT, sending command to NEEO');
 }
