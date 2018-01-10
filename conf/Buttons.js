7f88ba492a74256e64f05b521d42d97dbdb3327e
morgan.fromentin <morgan.fromentin@ezbeez.eu>
2018-01-10 19:39:44 +0100

    adding freebox and shield commands config

--- conf/Buttons.js
+++ conf/Buttons.js
@@ -18,22 +18,76 @@ module.exports = {
         label: "GREEN"
     },
     CURSOR_UP: {
-        class: "fa-angle-up white",
+        class: "fa-angle-up white margintop--5",
         label: "UP"
     },
     CURSOR_RIGHT: {
-        class: "fa-angle-right white",
+        class: "fa-angle-right white marginright--5",
         label: "RIGHT"
     },
     CURSOR_DOWN: {
-        class: "fa-angle-down white",
+        class: "fa-angle-down white margintop-5",
         label: "DOWN"
     },
     CURSOR_LEFT: {
-        class: "fa-angle-left white",
+        class: "fa-angle-left white marginleft--5",
         label: "LEFT"
     },
     CURSOR_ENTER: {
         label: "OK"
+    },
+    DIGIT_0: {
+        label: "0"
+    },
+    DIGIT_1: {
+        label: "1"
+    },
+    DIGIT_2: {
+        label: "2"
+    },
+    DIGIT_3: {
+        label: "3"
+    },
+    DIGIT_4: {
+        label: "4"
+    },
+    DIGIT_5: {
+        label: "5"
+    },
+    DIGIT_6: {
+        label: "6"
+    },
+    DIGIT_7: {
+        label: "7"
+    },
+    DIGIT_8: {
+        label: "8"
+    },
+    DIGIT_9: {
+        label: "9"
+    },
+    PREVIOUS: {
+        class: "fa-fast-backward white"
+    },
+    REVERSE: {
+        class: "fa-backward white"
+    },
+    PLAY: {
+        class: "fa-play white"
+    },
+    PAUSE: {
+        class: "fa-pause white"
+    },
+    FORWARD: {
+        class: "fa-forward white"
+    },
+    NEXT: {
+        class: "fa-fast-forward white"
+    },
+    MENU: {
+        label: "MENU"
+    },
+    BACK: {
+        label: "BACK"
     }
 }
